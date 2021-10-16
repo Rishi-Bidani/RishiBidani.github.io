a9b752f75c7a8bc8b40420be5b1a847c9e5e092e
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-10-16 16:09:10 +0530

    redesigned website

--- js/links.js
+++
@@ -1,7 +0,0 @@
-const cards = [...document.querySelectorAll(".card")];
-cards.map(card => {
-    card.setAttribute("title", `Go to ${card.getAttribute("data-link")}`)
-    card.onclick = () => {
-        window.location.href = card.getAttribute("data-link");
-    };
-})
---
+++ js/myJS.js
@@ -0,0 +1,2 @@
+const githubIcon = document.querySelector(".logo-item");
+githubIcon.onclick = () => window.location.href = githubIcon.getAttribute("data-link");
