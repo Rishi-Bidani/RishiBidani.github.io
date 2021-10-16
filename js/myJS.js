51db097437dc33251c75026f01ccc6eba5293138
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-10-16 18:13:17 +0530

    Added projects and some more - languages I have used - postgresql and python

--- js/myJS.js
+++ js/myJS.js
@@ -1,2 +1,8 @@
 const githubIcon = document.querySelector(".logo-item");
 githubIcon.onclick = () => window.location.href = githubIcon.getAttribute("data-link");
+
+const projects = [...document.querySelectorAll(".card")];
+projects.map(project => {
+    project.setAttribute("title", `Go to ${project.getAttribute("data-link")}`);
+    project.onclick = () => window.location.href = project.getAttribute("data-link");
+})
