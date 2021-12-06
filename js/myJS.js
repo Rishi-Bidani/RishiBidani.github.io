68321a5e8439ca6440d249841b6269d6fe48de43
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-12-07 00:37:53 +0530

    using github api to fetch my stars, previously hardcoded

--- js/myJS.js
+++ js/myJS.js
@@ -6,3 +6,17 @@ projects.map(project => {
     project.setAttribute("title", `Go to ${project.getAttribute("data-link")}`);
     project.onclick = () => window.location.href = project.getAttribute("data-link");
 })
+
+
+const starSpans = [...document.querySelectorAll(".github--stars")];
+const githubRepoData = (
+    async () => (await fetch(`https://api.github.com/users/Rishi-Bidani/repos`)).json()
+)();
+
+
+starSpans.map(async span => {
+    const data = await githubRepoData;
+    span.textContent = data.filter(
+        repo => repo.id === parseInt(span.getAttribute("data-github-repo-id"))
+    )[0].stargazers_count;
+})
